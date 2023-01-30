36cfc3fa961ba33dcd94d86b696d82a4649c7eb5
Miller <jazminemiller@rocketcentral.com>
2023-01-30 17:00:23 -0500

    Names

---
+++ names.js
@@ -0,0 +1,13 @@
+let name1 = "Henry";
+let name2 ="Matt";
+let name3 = "Ichabod Crane"; 
+
+console.log(name1.length);
+
+if (
+     name1.length=== name2.length && name2.length === name3.length &&  name1.length === name3.length) {
+    console.log(`All three names, ${name1}, ${name2}, and ${name3}, are the same length`);
+
+} else {
+    console.log("other solutions")
+}
